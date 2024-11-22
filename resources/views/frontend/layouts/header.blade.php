282f678dc04ff9afc78d2cec93dc8f00165ef1bf
ridwanadly32 <ridwanadly8@gmail.com>
2024-11-22 15:23:46 +0700

    ubah web

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -6,14 +6,14 @@
                 <div class="col-lg-6 col-md-12 col-12">
                     <!-- Top Left -->
                     <div class="top-left">
-                        <ul class="list-main">
+                        {{-- <ul class="list-main">
                             @php
                                 $settings=DB::table('settings')->get();
                                 
                             @endphp
                             <li><i class="ti-headphone-alt"></i>@foreach($settings as $data) {{$data->phone}} @endforeach</li>
                             <li><i class="ti-email"></i> @foreach($settings as $data) {{$data->email}} @endforeach</li>
-                        </ul>
+                        </ul> --}}
                     </div>
                     <!--/ End Top Left -->
                 </div>
@@ -21,7 +21,7 @@
                     <!-- Top Right -->
                     <div class="right-content">
                         <ul class="list-main">
-                        <li><i class="ti-location-pin"></i> <a href="{{route('order.track')}}">Track Order</a></li>
+                        {{-- <li><i class="ti-location-pin"></i> <a href="{{route('order.track')}}">Track Order</a></li> --}}
                             {{-- <li><i class="ti-alarm-clock"></i> <a href="#">Daily deal</a></li> --}}
                             @auth 
                                 @if(Auth::user()->role=='admin')
